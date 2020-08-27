80e43e3231306a3d6d446933a7f35af3b3e65619
ssheaks <sarah.sheaks@gmail.com>
2020-08-26 18:35:12 -0700

    invoke figher methods

--- fighter.lua
+++ fighter.lua
@@ -46,3 +46,27 @@ end
 ------------------------------------
 -- Create two fighter objects
 ------------------------------------
+blanka = Fighter:new({
+    name = "Blanka",
+    health = 100,
+    speed = 60
+})
+print("Object "..blanka.name.." was created.")
+
+chun_li = Fighter:new({
+    name = "Chun Li",
+    health = 100,
+    speed = 85
+})
+print("Object "..chun_li.name.." was created.")
+
+------------------------------------
+-- Call object methods
+------------------------------------
+blanka:light_punch()
+blanka:heavy_kick()
+blanka:special_attack()
+
+chun_li:light_punch()
+chun_li:heavy_kick()
+chun_li:special_attack()
